fb47668cc21dca8b9462552016f2b0ba8589ee1c
ihza <gautsa38@gmail.com>
2019-10-14 06:43:42 +0800

    belajar terus ya

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>IMS | Network</title>
+</head>
+<body>
+<h3>Silahkan Masukkan Username dan Passwrod</h3>
+    <table>
+        <tr>
+            <td>Username</td>
+            <td><input type="text" name="uname" required="required"></td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td><input type="text" name="password" required="required"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="submit" name="submit" value="Login"></td>
+        </tr>
+    </table>
+</body>
+</html>
